1a96392a7412f15309a7af9cb3e223b69ec0572a
aperanto-lefko <polarster@yandex.ru>
2025-05-19 13:00:05 +0300

    Разделение на закладки

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,12 +1,9 @@
-from PySide6.QtWidgets import QMainWindow, QMessageBox, QTableWidgetItem, QHeaderView, QAbstractItemView, QTableWidget
-from PySide6.QtCore import Qt
-
-from config.database_config import get_session
-from exception.exceptions import RespondentNotFoundException
+from PySide6.QtWidgets import QMainWindow
+from gui.ui_sleepecho_gui import Ui_MainWindow
 from repositories.respondent_repository import RespondentRepository
 from services.respondent_service import RespondentService
-from gui.ui_sleepecho_gui import Ui_MainWindow
-
+from gui.widgets.respondent_tab import RespondentsTab
+from config.database_config import get_session
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -16,156 +13,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Sleepecho")
 
         session = next(get_session())
-        self.respondent_service = RespondentService(RespondentRepository(session))
+        respondent_service = RespondentService(RespondentRepository(session))
 
-        self.initialize_table()
-
-        self.ui.all_respondents_btn.clicked.connect(self.load_all_respondents)
-        self.ui.add_resp_btn.clicked.connect(self.add_respondent)
-        self.ui.search_resp_btn.clicked.connect(self.search_resp_by_last_name)
-        self.ui.search_resp_by_id_btn.clicked.connect(self.search_resp_by_id)
-        self.ui.update_resp_btn.clicked.connect(self.update_resp)
-        self.ui.delete_resp_btn.clicked.connect(self.delete_resp)
-
-    def initialize_table(self):
-        self.ui.resp_tablewidget.setColumnCount(7)
-        self.ui.resp_tablewidget.setHorizontalHeaderLabels([
-            "ID", "First Name", "Last Name", "Email", "Gender", "Country", "Age"
-        ])
-        self.ui.resp_tablewidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.ui.resp_tablewidget.setSortingEnabled(True)
-        self.ui.resp_tablewidget.setSelectionBehavior(QTableWidget.SelectRows)
-
-    def load_all_respondents(self):
-        try:
-            respondents = self.respondent_service.get_respondents()
-            self.load_data(respondents)
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось загрузить респондентов:\n{str(e)}")
-
-    def load_data(self, respondents):
-        self.ui.resp_tablewidget.setRowCount(0)
-        self.ui.resp_tablewidget.clearContents()
-        self.ui.resp_tablewidget.setRowCount(len(respondents))
-
-        for row, respondent in enumerate(respondents):
-            item_id = QTableWidgetItem()
-            item_id.setData(Qt.DisplayRole, int(respondent.id))
-            self.ui.resp_tablewidget.setItem(row, 0, item_id)
-
-            self.ui.resp_tablewidget.setItem(row, 1, QTableWidgetItem(respondent.first_name))
-            self.ui.resp_tablewidget.setItem(row, 2, QTableWidgetItem(respondent.last_name))
-            self.ui.resp_tablewidget.setItem(row, 3, QTableWidgetItem(respondent.email))
-            self.ui.resp_tablewidget.setItem(row, 4, QTableWidgetItem(respondent.gender))
-            self.ui.resp_tablewidget.setItem(row, 5, QTableWidgetItem(respondent.country))
-
-            age_item = QTableWidgetItem()
-            age_item.setData(Qt.DisplayRole, int(respondent.age))
-            self.ui.resp_tablewidget.setItem(row, 6, age_item)
-
-    def add_respondent(self):
-        try:
-            data = self.get_respondent_form_data()
-            if data is None:
-                return
-            self.respondent_service.add_respondent(**data)
-            self.load_all_respondents()
-
-            self.ui.name_resp.clear()
-            self.ui.last_name_resp.clear()
-            self.ui.email_resp.clear()
-            self.ui.gender_box.setCurrentIndex(0)
-            self.ui.country_resp.clear()
-            self.ui.age_resp.clear()
-
-            QMessageBox.information(self, "Успех", "Респондент успешно добавлен")
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось добавить респондента:\n{str(e)}")
-
-    def update_resp(self):
-        try:
-            data = self.get_respondent_form_data()
-            if data is None:
-                return
-            resp_id = int(self.ui.id_resp_search.text())
-            respondent = self.respondent_service.update_respondent(resp_id, **data)
-            if respondent:
-                self.load_all_respondents()
-                QMessageBox.information(self, "Успех", "Респондент успешно обновлён")
-            self.load_all_respondents()
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось обновить респондента:\n{str(e)}")
-
-    def search_resp_by_last_name(self):
-        try:
-            last_name = self.ui.last_name_field_search.text()
-            if not last_name:
-                QMessageBox.warning(self, "Ошибка", "Заполните поле \"Фамилия\" корректно")
-                return
-            respondents = self.respondent_service.search_resp_by_last_name(last_name)
-            self.load_data(respondents)
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось найти респондента:\n{str(e)}")
-
-    def search_resp_by_id(self):
-        try:
-            resp_id_text = self.ui.id_resp_search.text()
-            if not resp_id_text:
-                QMessageBox.warning(self, "Ошибка", "Заполните поле \"id\" корректно")
-                return
-            resp_id = int(resp_id_text)
-            respondent = self.respondent_service.search_resp_by_id(resp_id)
-
-            self.ui.name_resp.setText(respondent.first_name)
-            self.ui.last_name_resp.setText(respondent.last_name)
-            self.ui.email_resp.setText(respondent.email)
-            self.set_gender(respondent.gender)
-            self.ui.age_resp.setText(str(respondent.age))
-            self.ui.country_resp.setText(respondent.country)
-        except RespondentNotFoundException as e:
-            QMessageBox.critical(self, "Ошибка", str(e))
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось найти респондента:\n{str(e)}")
-
-    def delete_resp(self):
-        try:
-            resp_id_text = self.ui.id_resp_delete.text()
-            if not resp_id_text:
-                QMessageBox.warning(self, "Ошибка", "Заполните поле \"id\" корректно")
-                return
-            resp_id = int(resp_id_text)
-            result = self.respondent_service.delete_resp_by_id(resp_id)
-            if result:
-               QMessageBox.information(self, "Успех", "Респондент успешно удален")
-            self.load_all_respondents()
-        except RespondentNotFoundException as e:
-            QMessageBox.critical(self, "Ошибка", str(e))
-        except Exception as e:
-            QMessageBox.critical(self, "Ошибка", f"Не удалось найти респондента:\n{str(e)}")
-    def get_respondent_form_data(self):
-        first_name = self.ui.name_resp.text()
-        last_name = self.ui.last_name_resp.text()
-        email = self.ui.email_resp.text()
-        gender = self.ui.gender_box.currentText()
-        country = self.ui.country_resp.text()
-        age_text = self.ui.age_resp.text()
-
-        if not (first_name and last_name and email and age_text.isdigit()):
-            QMessageBox.warning(self, "Ошибка", "Заполните все поля корректно")
-            return None
-        return {
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
-            "gender": gender,
-            "country": country,
-            "age": int(age_text)
-        }
-
-    def set_gender(self, gender):
-        if gender == "Male":
-            self.ui.gender_box.setCurrentText("Male")
-        elif gender == "Female":
-            self.ui.gender_box.setCurrentText("Female")
-        else:
-            self.ui.gender_box.setCurrentText(" ")
+        # подключаем таб вкладки
+        self.respondents_tab = RespondentsTab(self.ui, respondent_service)
---
+++ gui/widgets/respondent_tab.py
@@ -0,0 +1,124 @@
+from PySide6.QtWidgets import QWidget, QMessageBox, QTableWidgetItem, QHeaderView, QTableWidget
+from PySide6.QtCore import Qt
+from exception.exceptions import RespondentNotFoundException
+
+
+def create_int_item(value):
+    item = QTableWidgetItem()
+    item.setData(Qt.DisplayRole, value)
+    return item
+
+
+class RespondentsTab(QWidget):
+    def __init__(self, ui, respondent_service):
+        super().__init__()
+        self.ui = ui
+        self.respondent_service = respondent_service
+        self.setup_table()
+        self.connect_signals()
+
+    def setup_table(self):
+        self.ui.resp_tablewidget.setColumnCount(7)
+        self.ui.resp_tablewidget.setHorizontalHeaderLabels([
+            "ID", "First Name", "Last Name", "Email", "Gender", "Country", "Age"
+        ])
+        self.ui.resp_tablewidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.resp_tablewidget.setSelectionBehavior(QTableWidget.SelectRows)
+
+    def connect_signals(self):
+        self.ui.all_respondents_btn.clicked.connect(self.load_all)
+        self.ui.add_resp_btn.clicked.connect(self.add_respondent)
+        self.ui.update_resp_btn.clicked.connect(self.update_respondent)
+        self.ui.delete_resp_btn.clicked.connect(self.delete_respondent)
+        self.ui.search_resp_btn.clicked.connect(self.search_by_last_name)
+        self.ui.search_resp_by_id_btn.clicked.connect(self.search_by_id)
+
+    def load_all(self):
+        respondents = self.respondent_service.get_respondents()
+        self.load_table(respondents)
+
+    def load_table(self, respondents):
+        self.ui.resp_tablewidget.setRowCount(0)
+        self.ui.resp_tablewidget.setRowCount(len(respondents))
+        for row, r in enumerate(respondents):
+            self.ui.resp_tablewidget.setItem(row, 0, create_int_item(r.id))
+            self.ui.resp_tablewidget.setItem(row, 1, QTableWidgetItem(r.first_name))
+            self.ui.resp_tablewidget.setItem(row, 2, QTableWidgetItem(r.last_name))
+            self.ui.resp_tablewidget.setItem(row, 3, QTableWidgetItem(r.email))
+            self.ui.resp_tablewidget.setItem(row, 4, QTableWidgetItem(r.gender))
+            self.ui.resp_tablewidget.setItem(row, 5, QTableWidgetItem(r.country))
+            self.ui.resp_tablewidget.setItem(row, 6, create_int_item(r.age))
+        self.ui.resp_tablewidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.resp_tablewidget.setSortingEnabled(True)
+        self.ui.resp_tablewidget.setSelectionBehavior(QTableWidget.SelectRows)
+        self.ui.resp_tablewidget.setEditTriggers(QTableWidget.NoEditTriggers) #запрет редактирования
+    def add_respondent(self):
+        data = self.get_form_data()
+        if not data: return
+        self.respondent_service.add_respondent(**data)
+        self.load_all()
+        QMessageBox.information(self, "Успех", "Респондент добавлен")
+
+    def update_respondent(self):
+        try:
+            data = self.get_form_data()
+            if not data: return
+            resp_id = int(self.ui.id_resp_search.text())
+            self.respondent_service.update_respondent(resp_id, **data)
+            self.load_all()
+            QMessageBox.information(self, "Успех", "Респондент обновлён")
+        except Exception as e:
+            QMessageBox.critical(self, "Ошибка", str(e))
+
+    def delete_respondent(self):
+        try:
+            resp_id = int(self.ui.id_resp_delete.text())
+            if self.respondent_service.delete_resp_by_id(resp_id):
+                QMessageBox.information(self, "Успех", "Респондент удалён")
+                self.load_all()
+        except Exception as e:
+            QMessageBox.critical(self, "Ошибка", str(e))
+
+    def search_by_last_name(self):
+        last_name = self.ui.last_name_field_search.text()
+        if not last_name:
+            QMessageBox.warning(self, "Ошибка", "Введите фамилию")
+            return
+        respondents = self.respondent_service.search_resp_by_last_name(last_name)
+        self.load_table(respondents)
+
+    def search_by_id(self):
+        try:
+            resp_id = int(self.ui.id_resp_search.text())
+            r = self.respondent_service.search_resp_by_id(resp_id)
+            self.ui.name_resp.setText(r.first_name)
+            self.ui.last_name_resp.setText(r.last_name)
+            self.ui.email_resp.setText(r.email)
+            self.ui.gender_box.setCurrentText(r.gender)
+            self.ui.country_resp.setText(r.country)
+            self.ui.age_resp.setText(str(r.age))
+        except RespondentNotFoundException as e:
+            QMessageBox.critical(self, "Ошибка", str(e))
+        except Exception as e:
+            QMessageBox.critical(self, "Ошибка", str(e))
+
+    def get_form_data(self):
+        first_name = self.ui.name_resp.text()
+        last_name = self.ui.last_name_resp.text()
+        email = self.ui.email_resp.text()
+        gender = self.ui.gender_box.currentText()
+        country = self.ui.country_resp.text()
+        age_text = self.ui.age_resp.text()
+
+        if not (first_name and last_name and email and age_text.isdigit()):
+            QMessageBox.warning(self, "Ошибка", "Заполните все поля корректно")
+            return None
+
+        return {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "gender": gender,
+            "country": country,
+            "age": int(age_text),
+        }
